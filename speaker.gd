cd2d63202befd7b747af35cbbff004773afafca8
Luterad <100513212+Luterad@users.noreply.github.com>
2024-08-06 19:53:25 +0300

    Initial commit #2
    Data completely moved to GitHub!

--- speaker.gd
+++
@@ -1,9 +0,0 @@
-extends VSplitContainer
-
-var script_dialog_box: GDScript = load("res://addons/dialog_box/dialog_box.gd")
-
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	pass
