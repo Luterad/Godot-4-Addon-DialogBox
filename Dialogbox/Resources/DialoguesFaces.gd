0adfec520befa83104115b1e086dc0a6d8d3796f
Luterad <100513212+Luterad@users.noreply.github.com>
2024-12-07 22:49:57 +0300

    Branching

--- Dialogbox/Resources/DialoguesFaces.gd
+++ Dialogbox/Resources/DialoguesFaces.gd
@@ -1,4 +1,14 @@
+@tool
 class_name DialoguesFaces extends Resource
 
 @export var id: int
-@export var faces: Array[CompressedTexture2D]
+
+## If [code]true[/code], you'll see speaker's face if setted on current frame in [member faces].
+@export var use: bool = false:
+	set(value):
+		use = value
+		if !use and faces.size() > 0: faces.clear()
+
+## If [member use] is [code]true[/code] and setted for current [member id] and frame, shows speaker's face.
+@export var faces: Array[CompressedTexture2D]:
+	set(value): if use: faces = value
